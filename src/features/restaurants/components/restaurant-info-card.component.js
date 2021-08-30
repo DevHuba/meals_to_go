26e415f7248ea00b877fad07665d6743b7d13cf6
AleksandrS <ASagalajev92@gmail.com>
2021-08-30 20:28:37 +0300

    Add consistency in style ( space )

--- src/features/restaurants/components/restaurant-info-card.component.js
+++ src/features/restaurants/components/restaurant-info-card.component.js
@@ -1,7 +1,16 @@
 import React from "react";
 import styled from "styled-components/native";
-import { Text, StyleSheet } from "react-native";
+import { Text, StyleSheet, View } from "react-native";
 import { Card } from "react-native-paper";
+import { SvgXml } from "react-native-svg";
+
+//Components
+import { Spacer } from "../../../components/spacer/spacer.components.js";
+
+//Icon SVG
+import star from "../../../../assets/star.js";
+import openIcon from "../../../../assets/open.js";
+import closedIcon from "../../../../assets/closed.js";
 
 const RestaurantCard = styled(Card)`
   background-color: ${(props) => props.theme.colors.ui.quaternary};
@@ -12,30 +21,99 @@ const RestaurantCardCover = styled(Card.Cover)`
   background-color: ${(props) => props.theme.colors.ui.quaternary};
 `;
 
-const Title = styled(Text)`
-  padding: ${(props) => props.theme.sizes[1]};
+const Address = styled(Text)`
+  font-family: ${(props) => props.theme.fonts.body};
+  font-size: ${(props) => props.theme.fontSizes.caption};
   color: ${(props) => props.theme.colors.ui.primary};
 `;
 
+const Title = styled(Text)`
+  font-family: ${(props) => props.theme.fonts.heading};
+  font-size: ${(props) => props.theme.fontSizes.body};
+  color: ${(props) => props.theme.colors.ui.primary};
+`;
+
+const Info = styled.View`
+  padding: ${(props) => props.theme.space[3]};
+`;
+
+const Rating = styled.View`
+  flex-direction: row;
+  padding-top: ${(props) => props.theme.space[2]};
+  padding-bottom: ${(props) => props.theme.space[1]};
+`;
+
+const Section = styled.View`
+  flex-direction: row;
+`;
+
+const Temporarily = styled.Text`
+  margin-right: ${(props) => props.theme.space[3]};
+  color: green;
+`;
+
+const SectionEnd = styled.View`
+  flex-direction: row;
+  margin-left: auto;
+  padding-top: ${(props) => props.theme.space[2]};
+  padding-bottom: ${(props) => props.theme.space[1]};
+`;
+
 export const RestaurantInfoCard = ({ restaurant = {} }) => {
-  const {
+  let {
     name = "Some Restorant",
     icon,
     photos = [
       "https://www.2foodtrippers.com/wp-content/uploads/2017/05/Tallinn-Food-Guide-Lieb-Resto-Soup.jpg.webp",
     ],
     address = "test address",
-    isOpenNow = true,
-    rating = 4,
-    isClosedTemporarily,
+    isOpenNow = false,
+    rating = 3,
+    isClosedTemporarily = true,
   } = restaurant;
+
+  //Fix rating range
+  if (rating >= 5) {
+    console.log(rating);
+    rating = 5;
+  } else if (rating <= 0) {
+    rating = 1;
+  } else {
+    rating;
+  }
+
+  const ratingArray = Array.from(new Array(Math.round(rating)));
 
   return (
     <RestaurantCard elevation={5}>
       <RestaurantCardCover key={name} source={{ uri: photos[0] }} />
-      <Title>{name}</Title>
+      <Info>
+        <Title>{name}</Title>
+        <Section>
+          <Rating>
+            {ratingArray.map(() => (
+              <SvgXml xml={star} width={20} height={20} />
+            ))}
+          </Rating>
+          <SectionEnd>
+            <Spacer position="right" size="large">
+              {isClosedTemporarily && (
+                <>
+                  <Temporarily>TEMPORARILY</Temporarily>
+                </>
+              )}
+            </Spacer>
+            <Spacer position="left" size="large">
+              {isOpenNow ? (
+                <SvgXml xml={openIcon} width={20} height={20} />
+              ) : (
+                <SvgXml xml={closedIcon} width={20} height={20} />
+              )}
+            </Spacer>
+          </SectionEnd>
+        </Section>
+        <Address>{address}</Address>
+      </Info>
     </RestaurantCard>
   );
 };
-
-const styles = StyleSheet.create({});
--- src/features/restaurants/screens/restaurants.screen.js
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -39,5 +39,3 @@ export const RestaurantsScreen = () => (
     </RestaurantCard>
   </SafeArea>
 );
-
-const styles = StyleSheet.create({});
