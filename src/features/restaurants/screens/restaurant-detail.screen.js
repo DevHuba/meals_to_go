cd0be3fe4ce791af620059b4f3cd6df398d2322e
AleksandrS <ASagalajev92@gmail.com>
2021-09-25 22:32:58 +0300

    add list.accordions + items etc.

--- src/features/restaurants/screens/restaurant-detail.screen.js
+++ src/features/restaurants/screens/restaurant-detail.screen.js
@@ -1,14 +1,33 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, { useState } from "react";
+import { List } from "react-native-paper";
 
 import { RestaurantInfoCard } from "../components/restaurant-info-card.component";
 import { SafeArea } from "../../../components/utility/safe-area.component";
 
 export const RestaurantDetailScreen = ({ route }) => {
   const { restaurant } = route.params;
+
   return (
     <SafeArea>
-      <RestaurantInfoCard restaurant={restaurant} />
+      {/* <RestaurantInfoCard restaurant={restaurant} /> */}
+      <List.Section title="Accordions">
+        <List.Accordion
+          title="1"
+          left={(props) => <List.Icon {...props} icon="folder" />}
+        >
+          <List.Item title="1-1 " />
+          <List.Item title="1-2" />
+        </List.Accordion>
+        <List.Accordion
+          title="2"
+          left={(props) => <List.Icon {...props} icon="folder" />}
+        >
+          <List.Item title="2-1" />
+          <List.Item title="2-2" />
+        </List.Accordion>
+      </List.Section>
     </SafeArea>
   );
 };
+
+// breakfast ,lunch, dinner, drinks
