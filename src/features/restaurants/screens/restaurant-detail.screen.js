d89723b3ee15e27dabd56d454042c12f4ec213b7
AleksandrS <ASagalajev92@gmail.com>
2021-09-25 23:10:50 +0300

    Add mock data for restaurant details + scrolling

--- src/features/restaurants/screens/restaurant-detail.screen.js
+++ src/features/restaurants/screens/restaurant-detail.screen.js
@@ -1,31 +1,68 @@
 import React, { useState } from "react";
 import { List } from "react-native-paper";
+import { ScrollView } from "react-native";
 
 import { RestaurantInfoCard } from "../components/restaurant-info-card.component";
 import { SafeArea } from "../../../components/utility/safe-area.component";
 
 export const RestaurantDetailScreen = ({ route }) => {
+  const [breakfastExpanded, setBreakfastExpanded] = useState(false);
+  const [lunchExpanded, setLunchExpanded] = useState(false);
+  const [dinnerExpanded, setDinnerExpanded] = useState(false);
+  const [drinksExpanded, setDrinksExpanded] = useState(false);
+
   const { restaurant } = route.params;
 
   return (
     <SafeArea>
-      {/* <RestaurantInfoCard restaurant={restaurant} /> */}
-      <List.Section title="Accordions">
+      <RestaurantInfoCard restaurant={restaurant} />
+      <ScrollView>
         <List.Accordion
-          title="1"
-          left={(props) => <List.Icon {...props} icon="folder" />}
+          title="Breakfast"
+          left={(props) => <List.Icon {...props} icon="white-balance-sunny" />}
+          expanded={breakfastExpanded}
+          onPress={() => setBreakfastExpanded(!breakfastExpanded)}
         >
-          <List.Item title="1-1 " />
-          <List.Item title="1-2" />
+          <List.Item title="Breakfast Burrito" />
+          <List.Item title="Belgian Style Waffles" />
+          <List.Item title="Cinnamon Rolls" />
+          <List.Item title="Bagel with Cream Cheese" />
         </List.Accordion>
         <List.Accordion
-          title="2"
-          left={(props) => <List.Icon {...props} icon="folder" />}
+          title="Lunch"
+          left={(props) => <List.Icon {...props} icon="hamburger" />}
+          expanded={lunchExpanded}
+          onPress={() => setLunchExpanded(!lunchExpanded)}
         >
-          <List.Item title="2-1" />
-          <List.Item title="2-2" />
+          <List.Item title="Buddha Bowl" />
+          <List.Item title="Burrito Bowl" />
+          <List.Item title="Chicken and Asparagus Lemon Stir-Fry" />
+          <List.Item title="Kale, Spinach, and Pear Smoothie" />
         </List.Accordion>
-      </List.Section>
+        <List.Accordion
+          title="Dinner"
+          left={(props) => <List.Icon {...props} icon="food-variant" />}
+          expanded={dinnerExpanded}
+          onPress={() => setDinnerExpanded(!dinnerExpanded)}
+        >
+          <List.Item title="Hard tacos or tostadas" />
+          <List.Item title="Huevos rancheros" />
+          <List.Item title="Stir fry" />
+          <List.Item title="Meatballs and rice" />
+        </List.Accordion>
+        <List.Accordion
+          title="Drinks"
+          left={(props) => <List.Icon {...props} icon="bottle-wine" />}
+          expanded={drinksExpanded}
+          onPress={() => setDrinksExpanded(!drinksExpanded)}
+        >
+          <List.Item title="Coffee" />
+          <List.Item title="Tea" />
+          <List.Item title="Sprite" />
+          <List.Item title="Coke" />
+          <List.Item title="Fanta" />
+        </List.Accordion>
+      </ScrollView>
     </SafeArea>
   );
 };
