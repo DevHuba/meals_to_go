92ce99e2b5bbb9d34bc2780800f520e7207cbf2d
DevHuba <devhuba@gmail.com>
2021-10-07 00:06:15 +0300

    AsincStorage

--- src/services/favorites/favorites.context.js
+++ src/services/favorites/favorites.context.js
@@ -1,9 +1,27 @@
 import React, { createContext, useState } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export const FavoritesContext = createContext();
 
 export const FavoritesContextProvider = ({ children }) => {
   const [favorites, setFavorites] = useState([]);
+
+  const saveFavorites = async (value) => {
+    try {
+      await AsyncStorage.setItem("@storage_Key", value);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const loadFavorites = async (value) => {
+    try {
+      const jsonValue = JSON.stringify(value);
+      await AsyncStorage.setItem("@storage_Key", jsonValue);
+    } catch (e) {
+      // saving error
+    }
+  };
 
   const add = (restaurant) => {
     setFavorites([...favorites, restaurant]);
