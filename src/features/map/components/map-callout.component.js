d84c3c1e14cbea550adad8c277bc26785254969c
DevHuba <devhuba@gmail.com>
2021-10-13 01:20:53 +0300

    commit 2

--- src/features/map/components/map-callout.component.js
+++ src/features/map/components/map-callout.component.js
@@ -1,11 +1,6 @@
 import React from "react";
-import styled from "styled-components/native";
+import { CompactRestaurantInfo } from "../../../components/restaurant/compact-restaurant-info.component";
 
-const MyText = styled.Text``;
 export const MapCallout = ({ restaurant }) => (
-<<<<<<< HEAD
   <CompactRestaurantInfo isMap restaurant={restaurant} />
-=======
-  <MyText>{restaurant.name}</MyText>
->>>>>>> parent of 36ceb2d (Map marker polish + favorites context)
 );
