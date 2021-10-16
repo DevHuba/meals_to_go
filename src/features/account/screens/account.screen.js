cd0aafda8e95b97707c086ca68b8887be0d6daf6
DevHuba <devhuba@gmail.com>
2021-10-16 13:22:16 +0300

    auth screens

---
+++ src/features/account/screens/account.screen.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+export const AccountScreen = ({ route, navigation }) => {
+  <View>
+    <Text>AccountScreen</Text>
+  </View>;
+};
+
+const styles = StyleSheet.create({});
---
+++ src/services/authentication/authentication.service.js
@@ -0,0 +1,6 @@
+import { firebase } from "@firebase/app";
+import "@firebase/auth";
+
+export const loginRequest = (email, password) => {
+  firebase.auth().signInWithEmailAndPassword(email, password);
+};
