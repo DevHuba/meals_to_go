059a9da6cf6a3d7e8d297e5cb239caf06eca5473
DevHuba <devhuba@gmail.com>
2021-10-20 12:30:28 +0300

    add lottie example animation

--- src/features/account/screens/account.screen.js
+++ src/features/account/screens/account.screen.js
@@ -1,10 +1,12 @@
 import React from "react";
+import LottieView from "lottie-react-native";
 import {
   AccountBackground,
   AccountCover,
   MainContainer,
   MainButton,
   Title,
+  AnimationWrapper,
 } from "../components/account.styles";
 import { Spacer } from "../../../components/spacer/spacer.components";
 
@@ -12,6 +14,16 @@ export const AccountScreen = ({ navigation }) => {
   return (
     <AccountBackground>
       <AccountCover />
+      <AnimationWrapper>
+        <LottieView
+          key="animation"
+          autoPlay
+          loop
+          resizeMode="cover"
+          source={require("../../../../assets/watermelon.json")}
+        />
+      </AnimationWrapper>
+
       <Title>Meals To Go</Title>
       <MainContainer>
         <MainButton
