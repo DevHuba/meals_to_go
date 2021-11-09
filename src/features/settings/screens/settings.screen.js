6e44542b9bdfb24d81126bade2e42893aa3e6d1e
DevHuba <devhuba@gmail.com>
2021-11-09 19:26:23 +0200

    add user icon + user email + style

--- src/features/settings/screens/settings.screen.js
+++ src/features/settings/screens/settings.screen.js
@@ -1,7 +1,9 @@
 import React, { useContext } from "react";
 import styled from "styled-components/native";
-import { List } from "react-native-paper";
+import { List, Avatar } from "react-native-paper";
 //Components
+import { Spacer } from "../../../components/spacer/spacer.components";
+import { Text } from "../../../components/typography/text.component";
 import { SafeArea } from "../../../components/utility/safe-area.component";
 //Contexts
 import { AuthenticationContext } from "../../../services/authentication/authentication.context";
@@ -9,13 +11,23 @@ import { AuthenticationContext } from "../../../services/authentication/authenti
 const SettingsItem = styled(List.Item)`
   padding: ${(props) => props.theme.space[3]};
 `;
+const AvatarContainer = styled.View`
+  align-items: center;
+`;
 
 export const SettingsScreen = ({ navigation }) => {
   //Use context for logOut
-  const { onLogOut } = useContext(AuthenticationContext);
+  const { onLogOut, user } = useContext(AuthenticationContext);
 
   return (
     <SafeArea>
+      <AvatarContainer>
+        <Avatar.Icon size={180} icon="human" backgroundColor="tomato" />
+        <Spacer position="top" size="large">
+          <Text variant="label">{user.email}</Text>
+        </Spacer>
+      </AvatarContainer>
+
       <List.Section>
         <SettingsItem
           title="Favorites"
