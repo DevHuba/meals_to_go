0d305fabd49980a4bd2e93f518faa12e9a54a67a
DevHuba <devhuba@gmail.com>
2022-02-20 20:33:00 +0200

    add radius and elevation into favorites card

--- src/components/favorites/favorites-bar.component.js
+++ src/components/favorites/favorites-bar.component.js
@@ -1,12 +1,17 @@
 import React from "react";
 import { ScrollView, TouchableOpacity } from "react-native";
+import { Card } from "react-native-paper";
 import styled from "styled-components/native";
 import { Spacer } from "../spacer/spacer.components";
 import { CompactRestaurantInfo } from "../restaurant/compact-restaurant-info.component";
 import { Text } from "../typography/text.component";
 
-const FavoritesWrapper = styled.View`
+const FavoritesWrapper = styled(Card)`
   padding: 10px;
+  z-index: 999;
+  width: 95%;
+  align-self: center;
+  border-radius: 15px;
 `;
 
 export const FavoritesBar = ({ favorites, onNavigate }) => {
@@ -14,7 +19,7 @@ export const FavoritesBar = ({ favorites, onNavigate }) => {
     return null;
   }
   return (
-    <FavoritesWrapper>
+    <FavoritesWrapper elevation={3}>
       <Spacer>
         <Text variant="caption">Favorites</Text>
       </Spacer>
